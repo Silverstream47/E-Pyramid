302127b89370fb857accea1e516a6fdda3ceaa91
Silverstream47 <78374482+Silverstream47@users.noreply.github.com>
2021-02-11 11:59:03 -0800

    Create test.cs

---
+++ test.cs
@@ -0,0 +1,11 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+   Console.Writeline ("     A     ");
+   Console.Writeline ("    ABA    ");
+   Console.Writeline ("   ABCBA   ");
+   Console.Writeline ("  ABCDCBA  ");
+   Console.Writeline (" ABCDEDCBA ");
+  }
+}
