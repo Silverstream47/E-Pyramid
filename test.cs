4826fc5a222f156a0a1ad5e21f4b5fe2aee9cf85
Silverstream47 <78374482+Silverstream47@users.noreply.github.com>
2021-02-22 11:27:32 -0800

    Update test.cs

--- test.cs
+++ test.cs
@@ -2,10 +2,31 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-   Console.Writeline ("     A     ");
-   Console.Writeline ("    ABA    ");
-   Console.Writeline ("   ABCBA   ");
-   Console.Writeline ("  ABCDCBA  ");
-   Console.Writeline (" ABCDEDCBA ");
+   Console.Writeline ("                          E                          ");
+   Console.Writeline ("                         EEE                         ");
+   Console.Writeline ("                        EEEEE                        ");
+   Console.Writeline ("                       EEEEEEE                       ");
+   Console.Writeline ("                      EEEEEEEEE                      ");
+   Console.Writeline ("                     EEEEEEEEEEE                     ");
+   Console.Writeline ("                    EEEEEEEEEEEEE                    ");
+   Console.Writeline ("                   EEEEEEEEEEEEEEE                   ");
+   Console.Writeline ("                  EEEEEEEEEEEEEEEEE                  ");
+   Console.Writeline ("                 EEEEEEEEEEEEEEEEEEE                 ");
+   Console.Writeline ("                EEEEEEEEEEEEEEEEEEEEE                ");
+   Console.Writeline ("               EEEEEEEEEEEEEEEEEEEEEEE               ");
+   Console.Writeline ("              EEEEEEEEEEEEEEEEEEEEEEEEE              ");
+   Console.Writeline ("             EEEEEEEEEEEEEEEEEEEEEEEEEEE             ");
+   Console.Writeline ("            EEEEEEEEEEEEEEEEEEEEEEEEEEEEE            ");
+   Console.Writeline ("           EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE           ");
+   Console.Writeline ("          EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE          ");
+   Console.Writeline ("         EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE         ");
+   Console.Writeline ("        EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE        ");
+   Console.Writeline ("       EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE       ");
+   Console.Writeline ("      EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE      ");
+   Console.Writeline ("     EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE     ");
+   Console.Writeline ("    EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE    ");
+   Console.Writeline ("   EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE   ");
+   Console.Writeline ("  EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE  ");
+   Console.Writeline (" EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE ");                      
   }
 }
